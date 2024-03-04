6727e6313cb821f9f0c1d3211b0200f6c38dbc09
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-03-04 18:35:43 +0100

    Initial Commit

---
+++ SimpleBirthReminder/SimpleBirthReminderApp.swift
@@ -0,0 +1,32 @@
+//
+//  SimpleBirthReminderApp.swift
+//  SimpleBirthReminder
+//
+//  Created by Nicolò Curioni on 04/03/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SimpleBirthReminderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
